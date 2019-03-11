fa4cbf45db49314e9d7be2c66743df5a62647061
syifasalsabyla228@gmail.com <syifasalsabyla228@gmail.com>
2019-03-11 14:51:03 +0700

    Add project files.

---
+++ belajarLoginLagi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace belajarLoginLagi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
