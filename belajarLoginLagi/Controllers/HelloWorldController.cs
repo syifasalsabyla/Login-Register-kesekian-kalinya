81157160baf47abd3d6bdfbb6f4016a0290a8830
syifasalsabyla228@gmail.com <syifasalsabyla228@gmail.com>
2019-03-13 10:18:31 +0700

    Menambahkan fungsi dalam HelloWorldController

--- belajarLoginLagi/Controllers/HelloWorldController.cs
+++ belajarLoginLagi/Controllers/HelloWorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,10 @@ namespace belajarLoginLagi.Controllers
         public string Welcome()
         {
             return "This is the Welcome action method...";
+        }
+        public string Hi(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
